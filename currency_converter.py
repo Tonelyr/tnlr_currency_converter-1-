97cbe8f2490f3d02adf116dd3c38d6ecadf2e519
Tonelyr <40433952+Tonelyr@users.noreply.github.com>
2020-07-20 16:43:44 -0400

    Update currency_converter.py

--- currency_converter.py
+++ currency_converter.py
@@ -2,11 +2,11 @@ import requests
 import json
 
 print("Welcome to the Currency Converter !")
-user_currency = input("Convert from: ")
-user_desired_currency = input("Convert to: ")
-amount = float(input('Amount of ' + user_currency + " to convert to " + user_desired_currency + ": "))
+user_currency = input("Convert from: ") # Starting Currency
+user_desired_currency = input("Convert to: ") # Arrival Currency
+amount = float(input('Amount of ' + user_currency + " to convert to " + user_desired_currency + ": ")) # Amount to convert
 
-api_link = "https://api.exchangeratesapi.io/latest?base=" + user_currency
+api_link = "https://api.exchangeratesapi.io/latest?base=" + user_currency # API
 
 if type(amount) == float and type(user_currency) and type(user_desired_currency) == str:
     response = requests.get(api_link)
@@ -20,6 +20,6 @@ if type(amount) == float and type(user_currency) and type(user_desired_currency)
             print("1", user_currency, "=", currency, rate)
             print(amount, user_currency, "=", currency, conversion)
 else:
-    raise ValueError("Invalid Value, please check out our Available Currencies at https://pastebin.com/wV17F5wX")
-    raise KeyError("Invalid Value, please check out our Available Currencies at https://pastebin.com/wV17F5wX")
+    raise ValueError("Invalid Value, please check out our Available Currencies at https://pastebin.com/wV17F5wX") # Custom Errors
+    raise KeyError("Invalid Value, please check out our Available Currencies at https://pastebin.com/wV17F5wX") # Custom Errors
 # Pastebin of Available Currencies : https://pastebin.com/wV17F5wX
