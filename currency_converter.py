5985235cb33b2e9850780a227c99d36e4164c32f
Tonelyr <40433952+Tonelyr@users.noreply.github.com>
2020-07-20 16:42:28 -0400

    Add files via upload

--- currency_converter.py
+++ currency_converter.py
@@ -8,15 +8,18 @@ amount = float(input('Amount of ' + user_currency + " to convert to " + user_des
 
 api_link = "https://api.exchangeratesapi.io/latest?base=" + user_currency
 
-response = requests.get(api_link)
-data = response.text
-parsed = json.loads(data)
-rates = parsed["rates"]
+if type(amount) == float and type(user_currency) and type(user_desired_currency) == str:
+    response = requests.get(api_link)
+    data = response.text
+    parsed = json.loads(data)
+    rates = parsed["rates"]
 
-for currency, rate in rates.items():
-    if currency == user_desired_currency:
-        conversion = rate * amount
-        print("1", user_currency, "=", currency, rate)
-        print(amount, user_currency, "=", currency, conversion)
-
+    for currency, rate in rates.items():
+        if currency == user_desired_currency:
+            conversion = rate * amount
+            print("1", user_currency, "=", currency, rate)
+            print(amount, user_currency, "=", currency, conversion)
+else:
+    raise ValueError("Invalid Value, please check out our Available Currencies at https://pastebin.com/wV17F5wX")
+    raise KeyError("Invalid Value, please check out our Available Currencies at https://pastebin.com/wV17F5wX")
 # Pastebin of Available Currencies : https://pastebin.com/wV17F5wX
